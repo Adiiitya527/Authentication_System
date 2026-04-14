4ba48ad628de6245f2422231aba2cd1f200f3d94
Aditya <Adityanalawade527@gmail.com>
2026-04-14 22:03:45 +0530

    Middleware and me route

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -43,8 +43,6 @@ export default function ProfilePage() {
         onClick={getUserDetails}
         className="bg-green-800 mt-4 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >GetUser Details</button>
-
-
             </div>
     )
 }
